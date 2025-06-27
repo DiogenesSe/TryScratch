Fixed matrix confusion but training still gets wrong, probably need to average the errors like in batch size and not readjust the weights and biases instantly

main.py
@@ -27,14 +27,58 @@ def plot():
     fig.tight_layout()
     plt.show()
 
-weights = np.random.rand(3)
-eta = 0.2
+
+def abbilden():
+    x = -0.5
+    point_small  = np.empty((2,0))
+    solution = np.empty((1,0))
+    while x <= 1.5:
+        y = -0.5
+        while y <= 1.5:
+            point = np.array([[x], [y]])
+            point_small = np.hstack((point_small, point))
+            if anwenden(point.T) < 0.5:
+                solution = np.hstack((solution, np.array([[-1]])))
+            else:
+                solution = np.hstack((solution, np.array([[1]])))
+            y += 0.1
+        x += 0.1
+
+    label_colors = {-1: 'r', 1: 'g'}
+    colors = list(map(lambda x: label_colors[int(x)], solution.flatten()))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax.scatter(point_small[0, :], point_small[1, :], c=list(colors), s=60)
+    ax.set_xlim(-0.5, 1.5)
+    ax.set_ylim(-0.5, 1.5)
+    ax.set_aspect("equal")
+    fig.tight_layout()
+    plt.show()
 
 
 
-#plot()
-neuron = Node(2,1, 0.2, "sigmoid")
-for i in range(points.shape[1]):
-    pair = points[:, i]
-    res = neuron.forward(pair)
-    print(res)
+
+neuron = Node(2,4, 0.1, "sigmoid", "hidden")
+neuron2 = Node(4,1,0.1, "sigmoid", "output")
+
+def train(data,y):
+    hidden_1 = neuron.forward(data)
+    output = neuron2.forward(hidden_1)
+    output_delta = neuron2.backward(hidden_1,y,output,0,0)
+    hidden_1 = neuron.backward(data,y,hidden_1, output_delta,neuron2.getMatrix() )
+
+def anwenden(data):
+    hidden_1 = neuron.forward(data)
+    output = neuron2.forward(hidden_1)
+    return output
+
+plot()
+
+for x in range(1000):
+    for i in range(points.shape[1]):
+        pair = points[:, i]
+        train(pair, labels[i])
+        #print(res)
+abbilden()

node.py
@@ -2,21 +2,33 @@ import numpy as np
 import random
 
 class Node:
-    def __init__(self, X_input_size, data_output_size, eta, activation):
+    def __init__(self, X_input_size, data_output_size, eta, activation, position):
         self.data_input_size = X_input_size
         self.data_output_size = data_output_size
         self.eta = eta
+        # n x p has to be machted to p x y
         self.matrix = np.random.rand(X_input_size, data_output_size)
+        self.position = position
         #to avoid dying rlu 0.01 else 0.0 would be acceptable
-        self.bias = np.full((data_output_size,1),0.01)
+        self.bias = np.full((1,data_output_size),0.01)
         # dictionary for all the activaion function
         if isinstance(activation, str):
-            dic= {'sigmoid': self.sigmoid, 'relu': self.relu, 'sign': self.sign}
+            dic= {'sigmoid': (self.sigmoid, self.sigmoid_deriv), 'relu': (self.relu, self.relu_deriv), 'sign': (self.sign, self.sign_deriv) }
             if activation not in dic:
                 raise ValueError(f"Unknown activation: {activation}")
-            self.activation = dic[activation]
+            self.activation, self.activation_deriv = dic[activation]
+
         else:
             self.activation = activation
+
+    #Error Depending on position
+    def error(self, y, y_predict, output_delta, weights_hidden_output):
+        if self.position == "output":
+            return y - y_predict
+        elif self.position == "hidden":
+            return np.dot(output_delta, weights_hidden_output.T)
+        else:
+            return 0
 
     #tanh output x-> -inf : -1, x -> inf: 1
     #o(x) = (e^z - e ^ -z) / (e ^ z + e ^ -z)
@@ -42,6 +54,8 @@ class Node:
     #Posible output x= 0 then 0 if x >0 then 1 ELSE -1
     def sign(self, x):
         return np.sign(x)
+    def sign_deriv(self, x):
+        return 0
 
     #Good for classification problems. CE = -(y * log(ŷ)+(1-y)*log(1-ŷ))
     # y = real label, ŷ = prediction
@@ -58,13 +72,36 @@ class Node:
 
     #data x wheight_matrix + biase = z , activation(z) = prediction
     def forward(self, data):
+        data = data.reshape(1, -1)
         z = np.dot(data, self.matrix) + self.bias
         prediction = self.activation(z)
         return prediction
 
-    def backward(self, y ):
+    #gradient wheigt = learing rate * error term *
+    def backward(self, output_prev, y, y_predict, output_delta, weights_previous):
+        #error if output = y_predict -y else hidden = w_danach * y_predict
+        error = self.error(y, y_predict, output_delta, weights_previous)
+        #delta = error *f'(y_predict)
+        delta = error *  self.activation_deriv(y_predict)
+        #wheight = weight+ ( output_prev * error* f'(y_predict) * eta)
+        self.matrix += np.dot(output_prev.reshape(-1, 1) ,delta) * self.eta
+        #bias =
+        self.bias += np.sum(delta, axis=0, keepdims=True) * self.eta
+        return delta
 
-        pass
 
+    def getMatrix(self):
+        return self.matrix
+    def getBias(self):
+        return self.bias
 
+    def setBias(self, bias):
+        self.bias = bias
+    def setMatrix(self, matrix):
+        self.matrix = matrix
+    def returnAll(self):
+        print("---------- MATRIX ----------")
+        print(self.matrix)
+        print("----------- BIAS -----------")
+        print(self.bias)
 